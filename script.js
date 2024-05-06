21bc1f0c2125d0a5a3d8e636040637205ac139d8
Nick Bartlett <nickbartlett19@gmail.com>
2024-05-06 13:41:12 -0400

    add buttons to button-grid

--- script.js
+++ script.js
@@ -7,12 +7,33 @@ function toggleMode() {
         mathMode.classList.remove('hide');
         simpleMode.classList.add('hide');
     }
+
+    // if (buttonGrid.firstChild) {
+    //     renderButtons();
+    // }
 }
 
+function renderButtons() {
+    let button = document.createElement('div');
+    let text = document.createElement('span');
+    text.innerText = (Math.floor(Math.random()*100000009999)).toString();
+    button.appendChild(text);
+    button.classList.add('simple-button');
+    return button;
+}
 
 const toggleBtn = document.querySelector('.toggle-btn');
 const mathMode = document.querySelector('.math-mode');
 const simpleMode = document.querySelector('.simple-mode');
 
+const buttonGrid = document.querySelectorAll('.button-grid');
+
+// console.log(buttonGrid);
+
+for (let i = 0; i < 10; i++) {
+    console.log('rendering buttons!');
+    buttonGrid[0].appendChild(renderButtons());
+    buttonGrid[1].appendChild(renderButtons());
+}
 
 toggleBtn.addEventListener('click', () => toggleMode());
