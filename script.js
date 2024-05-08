d6775b68739f67bea2b5460ec03e582a210c3de0
Nick Bartlett <nickbartlett19@gmail.com>
2024-05-08 11:51:30 -0400

    add functions for rendering buttons, and appending them to button-grid

--- script.js
+++ script.js
@@ -13,13 +13,44 @@ function toggleMode() {
     // }
 }
 
-function renderButtons() {
+let basicButtons = [
+ '+', '-', 'x', '÷', '=', 'AC', '+/-', '%', '.',
+ '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'
+];
+
+// renderButton(basicButtons);
+
+// for (let button of basicButtons) {
+//     console.log(button);
+// }
+
+function renderButton(buttonText, buttonType) {
     let button = document.createElement('div');
     let text = document.createElement('span');
-    text.innerText = (Math.floor(Math.random()*100000009999)).toString();
+    text.innerText = buttonText;
     button.appendChild(text);
-    button.classList.add('simple-button');
+    button.classList.add(buttonType.toString());
     return button;
+    // for (let elem of buttons) {
+    //     let buttonDiv = document.createElement('div');
+    //     let buttonText = document.createElement('span');
+    //     buttonText.innerText = elem;
+
+    //     buttonDiv.appendChild(text);
+    //     buttonDiv.classList.add('simple-button');
+    // }
+
+    // button.appendChild(text);
+    // button.classList.add('simple-button');
+    // return button;
+}
+
+function appendButtons(buttons, HTMLelem) {
+    console.log('appending buttons!')
+    for (let elem of buttons) {
+        let button = renderButton(elem, 'simple-button');
+        HTMLelem.appendChild(button);
+    }
 }
 
 const toggleBtn = document.querySelector('.toggle-btn');
@@ -28,12 +59,9 @@ const simpleMode = document.querySelector('.simple-mode');
 
 const buttonGrid = document.querySelectorAll('.button-grid');
 
-// console.log(buttonGrid);
+appendButtons(basicButtons, buttonGrid[0]);
+appendButtons(basicButtons, buttonGrid[1]);
+appendButtons(basicButtons, buttonGrid[2]);
 
-for (let i = 0; i < 10; i++) {
-    console.log('rendering buttons!');
-    buttonGrid[0].appendChild(renderButtons());
-    buttonGrid[1].appendChild(renderButtons());
-}
 
 toggleBtn.addEventListener('click', () => toggleMode());
