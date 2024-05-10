e4138a1309a5128991dc97fcf5517d04e12958de
Nick Bartlett <nickbartlett19@gmail.com>
2024-05-10 13:45:36 -0400

    refactor calculator object into module

---
+++ main.js
@@ -0,0 +1,76 @@
+import {Calculator} from './modules/calculator.js';
+
+function toggleMode() {
+    if (simpleMode.classList.contains('hide')) {
+        simpleMode.classList.remove('hide');
+        mathMode.classList.add('hide');
+    }
+    else if (mathMode.classList.contains('hide')) {
+        mathMode.classList.remove('hide');
+        simpleMode.classList.add('hide');
+    }
+}
+
+function renderButton(buttonText, buttonType) {
+    let button = document.createElement('div');
+    let text = document.createElement('span');
+    text.innerText = buttonText;
+    button.appendChild(text);
+    button.classList.add(buttonType.toString());
+    return button;
+}
+
+function appendButtons(buttons, HTMLelem) {
+    for (let elem of buttons) {
+        let button = renderButton(elem, 'simple-button');
+        HTMLelem.appendChild(button);
+    }
+}
+
+// simple mode
+let numberButtonsArr = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '='];
+let mathButtonsArr = ['+', '-', 'x', '÷'];
+let calcButtonsArr = ['AC', '+/-', '%', '.'];
+
+// math mode
+let trigButtonsArr = ['Sin', 'Cos', 'Tan'];
+let advMathButtonsArr = ['a^x', 'log', 'ln'];
+
+const toggleBtn = document.querySelector('.toggle-btn');
+const mathMode = document.querySelector('.math-mode');
+const simpleMode = document.querySelector('.simple-mode');
+
+const buttonGrid = document.querySelectorAll('.button-grid');
+const numberButtons = document.querySelectorAll('.number-buttons');
+const mathButtons = document.querySelectorAll('.math-buttons');
+const calcButtons = document.querySelectorAll('.calc-buttons');
+
+const trigButtons = document.querySelectorAll('.trig-buttons');
+const advMathButtons = document.querySelectorAll('.adv-math-buttons');
+
+
+for (let elem of mathButtons) {
+    appendButtons(mathButtonsArr, elem);
+}
+
+for (let elem of calcButtons) {
+    appendButtons(calcButtonsArr, elem);
+}
+
+for (let elem of numberButtons) {
+    appendButtons(numberButtonsArr, elem);
+}
+
+for (let elem of trigButtons) {
+    appendButtons(trigButtonsArr, elem);
+}
+
+for (let elem of advMathButtons) {
+    appendButtons(advMathButtonsArr, elem);
+}
+
+toggleBtn.addEventListener('click', () => toggleMode());
+
+let myCalc = new Calculator();
+
+// console.log(myCalc.firstOperand);
---
+++ modules/calculator.js
@@ -0,0 +1,7 @@
+export class Calculator {
+    constructor(op1, op2, oper) {
+        this.firstOperand = op1;
+        this.secondOperand = op2;
+        this.operator = oper;
+    }
+}
